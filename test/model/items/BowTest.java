b2096c1e206cbab51ed7502fd4053444fe136749
islaterm <ignacio.slater@ug.uchile.cl>
2019-07-27 21:35:48 -0400

    Test set for bows

---
+++ test/model/items/BowTest.java
@@ -0,0 +1,32 @@
+package model.items;
+
+/**
+ * Test set for bows
+ *
+ * @author Ignacio Slater Muñoz
+ * @since 1.0
+ */
+public class BowTest extends AbstractTestItem {
+
+  private Bow bow;
+
+  /**
+   * Sets which item is going to be tested
+   */
+  @Override
+  public void setTestItem() {
+    expectedName = "Common bow";
+    expectedBaseDamage = 8;
+    expectedMinRange = 2;
+    expectedMaxRange = 4;
+    bow = new Bow(expectedName, expectedBaseDamage, expectedMinRange, expectedMaxRange);
+  }
+
+  /**
+   * @return the item being tested
+   */
+  @Override
+  public IEquipableItem getTestItem() {
+    return bow;
+  }
+}
